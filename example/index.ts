d82a4907cb144d24877e6f5ff1b9b3066c65b501
houxuelin <17857330861@163.com>
2026-03-15 17:03:40 +0800

    v0.0.1

--- example/index.ts
+++ example/index.ts
@@ -3,7 +3,7 @@ import { exportToPdf } from './pdfExport';
 const inputEl = document.querySelector('textarea') as HTMLTextAreaElement;
 const exportBtn = document.getElementById('exportBtn') as HTMLButtonElement;
 
-exportBtn.addEventListener('click', () => {
+exportBtn.addEventListener('click', async () => {
   const content = inputEl.value;
-  exportToPdf({ content, fileName: 'example.pdf' });
+  await exportToPdf({ content, fileName: 'example.pdf' });
 });
